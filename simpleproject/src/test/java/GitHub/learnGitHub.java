f090ced2a107ca6739b1f599db527e67d6b691fa
Chetan <Chetan@LAPTOP-D0QEJMKI>
2025-02-01 10:55:32 +0530

    first attempt

---
+++ simpleproject/src/test/java/GitHub/learnGitHub.java
@@ -0,0 +1,12 @@
+package GitHub;
+
+import org.testng.Reporter;
+
+import com.aventstack.extentreports.model.Report;
+
+public class learnGitHub {
+
+	public void learn() {
+		Reporter.log("github class added", true);
+	}
+}
